7560737e9ee3252dfa3f4d05ed310672fabc269f
Joshua <Doerak1aug@gmail.com>
2021-05-25 11:54:28 +0200

    scroll styling

--- Main.php
+++ Main.php
@@ -1,9 +1,178 @@
 <!DOCTYPE html>
 <html lang="nl">
     <head>
+        <title>CheckList</title>
+        <script src="https://kit.fontawesome.com/89f724c9b8.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <div id="mainContainer">
+            <div id="titleContainer">
+                <div id="mainTitle">Front-end-challenge</div>
+            </div>
+            <div id="listsContainer">
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
 
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                <div class="list">
+                    <div>
+                        <div class="listName">name</div>
+                        <div></div>
+                    </div>
+                    <ul class="listitems">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    <div>
+                        <button>+ Voeg een kaart toe</button>
+                    </div>
+                </div>
+                
+                
+                <button id="buttonCreateList" class="list">
+                    <div class="listName">+ name</div>
+
+                </button>
+            </div>
+        </div>
     </body>
 </html>
+<!-- seperate the php in visual and functional  -->

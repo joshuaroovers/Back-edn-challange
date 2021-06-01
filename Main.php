a35c20a854b2908c8904c069710593785d826220
Joshua <Doerak1aug@gmail.com>
2021-06-01 12:16:25 +0200

    poging tot styling en fuction of create list box

--- Main.php
+++ Main.php
@@ -4,6 +4,7 @@
         <title>CheckList</title>
         <script src="https://kit.fontawesome.com/89f724c9b8.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="style.css">
+        <script src='javascript.js'></script>
     </head>
     <body>
         <div id="mainContainer">
@@ -44,7 +45,7 @@
                         <div></div>
                     </div>
                     <ul class="listitems">
-                        <li>1</li>
+                        <li>1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50</li>
                         <li>2</li>
                         <li>3</li>
                     </ul>
@@ -67,13 +68,11 @@
                         <button>+ Voeg een kaart toe</button>
                     </div>
                 </div>
-                
-                
+                           
                 
                 
                 <button id="buttonCreateList" class="list">
                     <div class="listName">+ name</div>
-
                 </button>
             </div>
         </div>
---
+++ javascript.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("buttonCreateList").onclick = function(){
+    document.getElementById("buttonCreateListActive").firstChild.style.display = 'none';
+}/* why is firstChild different in css compared to js I am going to scream */

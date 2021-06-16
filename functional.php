8159b95b0cf45e3676d2d0688819852944314d90
Joshua <Doerak1aug@gmail.com>
2021-06-16 12:01:31 +0200

    ^^

--- functional.php
+++ functional.php
@@ -7,14 +7,15 @@ $password = "";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception
+ 
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 
 
-$conn->prepare("CREATE TABLE `LISTNAME` ( `id` INT NOT NULL AUTO_INCREMENT , `name` TEXT NOT NULL , `description` TEXT NOT NULL , `duration` INT NOT NULL , `labels` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-
+$test = $conn->prepare("CREATE TABLE `LISTNAME` ( `id` INT NOT NULL AUTO_INCREMENT , `name` TEXT NOT NULL , `description` TEXT NOT NULL , `duration` INT NOT NULL , `labels` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+$test->execute();
 
 include 'Main.php';
 ?>

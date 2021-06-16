836220654ae492801ad3ba800bf7a203c6a783ee
Joshua <Doerak1aug@gmail.com>
2021-06-16 11:51:15 +0200

    start actual php/sql

---
+++ functional.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$database = "back-end-checklist";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+$conn->prepare("CREATE TABLE `LISTNAME` ( `id` INT NOT NULL AUTO_INCREMENT , `name` TEXT NOT NULL , `description` TEXT NOT NULL , `duration` INT NOT NULL , `labels` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+
+
+include 'Main.php';
+?>
+
